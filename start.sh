384de037807b4c9106c44a3ed8b473f89d854584
Raymund Tienzo <mon2x0@gmail.com>
2026-01-02 20:39:58 +1300

    feat: add git submodule for ops-scripts

--- start.sh
+++ start.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Starting Poseidon in Production mode..."
+source ./scripts/load_env.sh
 
-# Ensure clean state
-docker stack rm poseidon || true
-# Wait for services to shutdown
-echo "Waiting for stack to be removed..."
-while docker stack ls | grep -q "poseidon"; do
-    echo "Stack still active, waiting..."
-    sleep 2
-done
-echo "Stack removed."
-
-# Start the services
-docker stack deploy --prune -c docker-compose.yml poseidon
-
-echo "Production environment deployed successfully."
+# Usage: ./scripts/deploy.sh <STACK_NAME> [COMPOSE_FILES...]
+./scripts/deploy.sh --skip-build "poseidon" docker-compose.yml
